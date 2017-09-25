640ae5a0f50b3f0f9e6e10f726770b092c50fb93
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-09-25 17:54:31 +0200

    model can now be a parameter

--- server/prediction.py
+++ server/prediction.py
@@ -12,13 +12,18 @@ import keras
 assert StrictVersion(keras.__version__) >= StrictVersion('2.0.0')
 print(keras.__version__)
 
-import numpy as np
 from keras.models import load_model
 
-model = load_model('models/conv-vgg.hdf5')
-# model = load_model('models/conv-vgg.h5')
-# model = load_model('models/conv-residual.h5')
-print ("Model loaded")
+import numpy as np
+
+loaded_models = {
+    'default': load_model('models/conv-vgg.hdf5'),
+    'augmented': load_model('models/conv-vgg-augmented.hdf5')
+}
+def name_to_model(model_name):
+    return loaded_models[model_name]
+
+print ("Models loaded")
 
 import skimage.data
 import skimage.transform
@@ -26,16 +31,16 @@ import skimage.transform
 def transform_to_image(file):
     # turn into image
     image = skimage.data.imread(file)
-    print(image.shape)
+    print('Raw Input Image Shape:', image.shape)
     # Resize images
     image64 = skimage.transform.resize(image, (64, 64))
-    print(image64.shape)
+    print('Resized Input Image Shape:', image64.shape)
     return image64
 
-
-def predict(image):
+def predict(image, model_name='default'):
     # normalize
     X_sample = np.array([image])
+    model = name_to_model(model_name)
     prediction = model.predict(X_sample)
     predicted_category = np.argmax(prediction, axis=1)
     return predicted_category, prediction
